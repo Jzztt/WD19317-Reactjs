923041ad5d9dae948c7270b50ae6c6035fa1e048
Jettsyy <teazyyy2512@gmail.com>
2025-03-18 20:25:13 +0700

    feat: create product use mutation

--- src/pages/CreateProduct.tsx
+++ src/pages/CreateProduct.tsx
@@ -1,15 +1,57 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { Button, Form, Input, Select } from "antd";
+import axios from "axios";
+import { redirect } from "react-router";
 
 const CreateProduct = () => {
-    const [form] = Form.useForm();
+  const [form] = Form.useForm();
+  const queryClient = useQueryClient();
+  const createProduct = async (payload) => {
+    try {
+      const { data } = await axios.post(
+        "http://localhost:3000/products",
+        payload
+      );
+      return data;
+    } catch (error) {
+        console.log(error);
+
+    }
+  };
+  const mutation = useMutation({
+    mutationFn: createProduct,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["products"] });
+      redirect("/admin/product");
+    },
+  });
+  const handleSubmit = (values) => {
+    const payload = {
+      name: values.name,
+      price : values.price,
+      rate: {
+        count: values.ratePoint,
+        star: values.rateStar
+      },
+      category: values.category,
+      image: values.image
+    }
+    mutation.mutate(payload);
+  };
+
   return (
     <>
       <div>CreateProduct</div>
       <div style={{ display: "flex", justifyContent: "center", width: "100%" }}>
-        <Form form={form} labelCol={{ span: 4 }} layout="horizontal" style={{ width: 800 }} onFinish={(values)=> console.log(values)}
+        <Form
+          form={form}
+          labelCol={{ span: 4 }}
+          layout="horizontal"
+          style={{ width: 800 }}
+          onFinish={handleSubmit}
         >
-          <Form.Item  name="name"  label="Name">
-            <Input  />
+          <Form.Item name="name" label="Name">
+            <Input />
           </Form.Item>
           <Form.Item name="price" label="Price">
             <Input />
