2e838b3800ee61116316b14c723b59fba169fe26
Jettsyy <teazyyy2512@gmail.com>
2025-03-20 19:08:10 +0700

    feat: delete product

--- src/pages/Product.tsx
+++ src/pages/Product.tsx
@@ -1,9 +1,28 @@
-import { useQuery } from "@tanstack/react-query";
-import { Button, Table } from "antd";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { Button, message, Popconfirm, Table } from "antd";
 import axios from "axios";
 import { NavLink } from "react-router";
+import { IProduct } from "./CreateProduct";
 
 const Product = () => {
+  const queryClient = useQueryClient();
+  const deleteProduct = async (id: number) => {
+    try {
+      const { data } = await axios.delete(
+        `http://localhost:3000/products/${id}`
+      );
+      return data;
+    } catch (error) {
+      console.log(error);
+    }
+  };
+   const mutation = useMutation({
+    mutationFn:deleteProduct,
+    onSuccess:() => {
+      message.success("Delete success");
+      queryClient.invalidateQueries({ queryKey: ["products"]})
+    }
+   })
   const fetchProducts = async () => {
     try {
       const { data } = await axios.get("http://localhost:3000/products");
@@ -19,6 +38,11 @@ const Product = () => {
   });
 
   const columns = [
+    {
+      title: "Id",
+      dataIndex: "id",
+      key: "id",
+    },
     {
       title: "Name",
       dataIndex: "name",
@@ -38,19 +62,26 @@ const Product = () => {
     {
       title: "Action",
       key: "action",
-      render: () => (
+      render: (record: IProduct) => (
         <div>
           <Button type="primary">Edit</Button>
-          <Button color="danger" variant="solid">
-            Delete
-          </Button>
+          <Popconfirm
+            title="Are you sure to delete this product?"
+            onConfirm={() => mutation.mutate(record.id)}
+          >
+            <Button color="danger" variant="solid">
+              Delete
+            </Button>
+          </Popconfirm>
         </div>
       ),
     },
   ];
   return (
     <>
-      <Button type="primary"><NavLink to={"/admin/product/add"}>Add Product</NavLink></Button>
+      <Button type="primary">
+        <NavLink to={"/admin/product/add"}>Add Product</NavLink>
+      </Button>
       <Table
         dataSource={products}
         columns={columns}
